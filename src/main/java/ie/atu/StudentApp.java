e31747d9916798a6ba19e9229cafe16e0395184c
Patrick <g00409592@atu.ie>
2022-12-11 21:11:59 +0000

    Changed spacing

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -37,12 +37,8 @@ public class StudentApp {
         temp_course = userInput.nextLine();
         Student stu3 = new Student(temp_name, temp_email, temp_course);
 
-
         System.out.println("Student1:\n" + stu1.getName() + "\n" + stu1.getEmail() + "\n" + stu1.getCourse() + "\n");
         System.out.println("Student2:\n" + stu2.getName() + "\n" + stu2.getEmail() + "\n" + stu2.getCourse() + "\n");
         System.out.println("Student2:\n" + stu3.getName() + "\n" + stu3.getEmail() + "\n" + stu3.getCourse() + "\n");
-
-
-
     }
 }
